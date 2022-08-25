05bf39bac218a7da98a732f962e1e67de767bc86
alieulaye <alioulaye5@gmail.com>
2022-08-25 08:46:41 +0000

    ajouter pour conserver l'espace machine

---
+++ admin/produit/views.py
@@ -0,0 +1,51 @@
+from itertools import product
+from logging import raiseExceptions
+from .models import Produit
+from .serializers import ProductSerializer
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+
+class ProductViewSet(viewsets.ViewSet):
+
+    def list(self,request):#/api/products
+        products = Produit.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+
+   
+   
+    def  create(self,request): #/api/products
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception= True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+   
+   
+   
+    def retrieve( self, request,pk:None):#/api/productcs/<str:id>
+        product = Produit.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+         
+    
+    
+    
+    
+    def update( self, request,pk:None):#/api/productcs/<str:id>
+        
+        product = Produit.objects.get(id=pk)
+        serializer= ProductSerializer(instance = product, data= request.data)
+        serializer.is_valid(raise_exception= True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_202_ACCEPTED)
+
+
+    
+    
+    
+    def destroy( self, request,pk:None):#/api/productcs/<str:id>
+        
+        product = Produit.objects.get(id=pk)
+        product.delete()
+        return Response(status =  status.HTTP_204_NO_CONTENT) 
